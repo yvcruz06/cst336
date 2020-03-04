cdae3ae228af89b2665ecdd2f5b1d0d83a992160
EC2 Default User <ec2-user@ip-172-31-16-176.us-west-2.compute.internal>
2020-03-04 21:46:53 +0000

    clean up and midterm files

--- public/ajax-json/app.js
+++ public/ajax-json/app.js
@@ -1,47 +1,26 @@
-/* Get DOM elements */
-var text           = $("#details-text");
-var moreDetailsBtn = $("#more-details-btn");
-var scdDetailsBtn  = $("#scd-details-btn");
-
-/* AJAX with vanilla JavaScript */
-var request = new XMLHttpRequest();
-request.onreadystatechange = function(){
-  if(request.readyState === 4){    
-    if(request.status === 200){
-      text.html(request.responseText);
-      text.addClass("details");
-    }
-    else{
-      text.html("An error occurred during your request: " +  request.status + " " + request.statusText);
-      text.addClass("error");
-    }
-  }
+let getEarthquakes = function(){
+	$.ajax({
+		url: "http://earthquake.usgs.gov/fdsnws/event/1/query",
+		type: "GET",
+		dataType: "json",
+		data: {
+			"format"   : "geojson",
+            "starttime": $("#starttime").val(),
+            "endtime"  : $("#endtime").val(),
+            "latitude" : $("#latitude").val(),
+            "longitude": $("#longitude").val(),
+            "maxradius": $("#maxradius").val(),
+            "minmag"   : $("#minmag").val()
+		},
+		success: function(data) {
+			$("#results").html("<h4>Results</h4><br/>");
+			data.features.forEach(function(feature){
+				$("#results").append(feature.properties.mag + " - " + feature.properties.place + "<br/>");
+			});
+			$("#results").addClass("details");
+		},
+    	error: function(error) {
+    		console.log(error);
+    	}
+	});
 }
-
-if ()
-
-request.open("GET", "https://jsonplaceholder.typicode.com/todos");
-
-moreDetailsBtn.on("click", function(){
-  request.send();  // NOTE: Must send if using AJAX with vanilla JS
-});
-
-/* AJAX with JQuery */
-scdDetailsBtn.on("click", function(){
-  $("img").attr("src", "data/scd.jpg");
-  $(this).hide();
-  $.ajax({
-    url: "data/scd.txt",
-    type: "GET",
-    dataType: "text",
-    success: function(data) {
-      text.html(data);
-      text.addClass("details");
-    },
-    error: function(e) {
-      text.html("An error occurred during your request: " +  e.status + " " + e.statusText);
-      text.addClass("error");
-    }
-  });
-});
-
