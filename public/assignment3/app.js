95914eab19b9a86cfc885379b2ed31b0c9261d72
EC2 Default User <ec2-user@ip-172-31-16-176.us-west-2.compute.internal>
2020-03-09 12:33:29 +0000

    assignment 3 update

---
+++ public/assignment3/app.js
@@ -0,0 +1,28 @@
+$("#search_input").submit(function(event){
+            $("#images").empty();
+            
+            var query = $("#kw").val()
+            var client_id = "-57k3dL4kpCuSx7fk2PsT8laCYccuiftTKRZf43RzTk";
+            var urllink = "https://api.unsplash.com/search/photos?&client_id="+client_id+"&page=1+&query="+query;
+            
+            $.ajax({
+                method: "GET",
+                url: urllink, 
+                dataType: "json",
+                data: { "query" : query
+		              },
+                success: function(data) {
+                    console.log(data.results);
+                    //console.log(data.results[0].urls.full);
+                    data.results.forEach(function(link){
+                        var img = "<div class='img-thumbnail flex-item'><img src='" + link.urls.small + "'></div>";
+                        var like = "<span class='likebox'>Likes: "+link.likes+"</span>";
+                        $("#images").append(img).append(like);
+                    });
+                },
+                error: function() {
+                    alert("error");
+                }
+            });
+        event.preventDefault();
+        });
